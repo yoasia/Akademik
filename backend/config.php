90d662258d25ebc69ffdccd869ef26726d10d61c
yoasien <asiasien@gmail.com>
2017-06-12 00:54:41 +0200

    Log-in files

---
+++ backend/config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * These are the database login details
+ */  
+define("HOST", "54.93.46.73");     // The host you want to connect to.
+define("USER", "asia");    // The database username. 
+define("PASSWORD", "cffe819d4413b95dd8c35c0085930789");    // The database password. 
+define("DATABASE", "project_1");    // The database name.
+ 
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+ 
+define("SECURE", FALSE);    // FOR DEVELOPMENT ONLY!!!!
+
+?>
