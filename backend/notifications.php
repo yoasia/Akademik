1c5bb15afb7eae9f7760c78e68426053214e3ed2
Darooks <karol.jurczenia@gmail.com>
2018-02-04 16:45:24 +0100

    Fixed typo in function definition

--- backend/notifications.php
+++ backend/notifications.php
@@ -1,25 +1,31 @@
 <?php
 include 'utils.php';
 
-  function() getNotifications {
+  function getNotifications()
+  {
     require('db_connect.php');
 
     $query = "SELECT * FROM notifications";
     $result = mysqli_query($connection, $query) or die(mysqli_error($connection));
     $json_array = array();
-    if($result > 0) {
-      //$result = json_encode($result);
-      while($row = mysqli_fetch_assoc($result)) {
+    if($result > 0)
+    {
+      while($row = mysqli_fetch_assoc($result))
+      {
         $row["nickname"] = getNickname($row["id_user"]);
         array_push($json_array, $row);
       }
+
       $json_array = json_encode($json_array);
 
-    } else {
+    }
+    else
+    {
       echo "blad";
     }
 
     return $json_array;
   }
+
 
  ?>
