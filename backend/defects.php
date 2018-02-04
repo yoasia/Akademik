4989320056fd77dc987201be578044eaffdf059b
Darooks <karol.jurczenia@gmail.com>
2018-02-04 17:11:14 +0100

    Add defects functionality

--- backend/defects.php
+++ backend/defects.php
@@ -24,4 +24,27 @@ include 'utils.php';
      return $json_array;
    }
 
+   function addDefects() {
+     require('db_connect.php');
+
+     $id_user = $_POST["id_user"];
+     $title = $_POST["title"];
+     $description = $_POST["description"];
+     $is_done = false;
+
+     $query = "INSERT INTO defects (id_user, title, description, is_done)
+      VALUES ('$id_user', '$title', '$description', '$is_done')";
+
+     if(mysqli_query($connection, $query) or die(mysqli_error($connection)))
+     {
+       return true;
+     }
+     else
+     {
+       return false;
+     }
+
+     return true;
+   }
+   
  ?>
