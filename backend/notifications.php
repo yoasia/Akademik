77eeba3754de21f09eb1a9f1596fb8dfc0182f29
Darooks <karol.jurczenia@gmail.com>
2018-02-04 17:01:18 +0100

    Add notification functionality

--- backend/notifications.php
+++ backend/notifications.php
@@ -1,5 +1,5 @@
 <?php
-include 'utils.php';
+  include 'utils.php';
 
   function getNotifications()
   {
@@ -27,5 +27,27 @@ include 'utils.php';
     return $json_array;
   }
 
+
+  function addNotification()
+  {
+    require('db_connect.php');
+
+    $notification = array();
+    $id_user = $_POST["id_user"];
+    $content = $_POST["content"];
+
+    $query = "INSERT INTO notifications (id_user, content) VALUES ('$id_user', '$content')";
+
+    if(mysqli_query($connection, $query) or die(mysqli_error($connection)))
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+
+    return true;
+  }
 
  ?>
