3d7ab9115749a668596cf880c8677e44c544e651
Darooks <karol.jurczenia@gmail.com>
2018-02-06 21:20:05 +0100

    Fixed login, should work properly

--- api/login/login.php
+++ api/login/login.php
@@ -6,29 +6,31 @@
   sec_session_start(); // Our custom secure way of starting a PHP session.
 
   if (isset($_GET['email'], $_GET['p'])) {
-
+      $email = $_GET['email'];
+      $password = $_GET['p'];
       if (login($email, $password) == true) {
           // Login success
-          $query = "SELECT room_number from users where email='$email'";
+          $query = "SELECT room_number FROM users WHERE email='$email'";
           $result = mysqli_query($mysqli, $query) or die(mysqli_error($mysqli));
-          
+
           if ($result > 0) {
                 $room_number = mysqli_fetch_assoc($result)["room_number"];
-                
+
                 $_SESSION["status"] = true;
                 $_SESSION["floor"] = getFloorNumber($room_number);
                 $_SESSION["mail"] = $email;
                 $_SESSION["nickname"] = getNicknameByMail($email);
                 $_SESSION["type"] = "student";
                 $_SESSION["room"] = $room_number;
-                
-                $query = "SELECT ds_number from users where email='$email'";
+
+                $query = "SELECT ds_number FROM users WHERE email='$email'";
                 $result_2 = mysqli_query($mysqli, $query) or die(mysqli_error($mysqli));
                 $_SESSION["ds_number"] = mysqli_fetch_assoc($result_2)["ds_number"];
 
-                $query = "SELECT id_user from users where email='$email'";
+                $query = "SELECT id_user FROM users WHERE email='$email'";
                 $result_3 = mysqli_query($mysqli, $query) or die(mysqli_error($mysqli));
                 $_SESSION["id_user"] = mysqli_fetch_assoc($result_3)["id_user"];
+
                 include_once './get_login_status.php';
 
           } else {
