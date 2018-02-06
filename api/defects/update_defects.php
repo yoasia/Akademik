f52bb776991961070c95bd45c9a141f9c08ed64a
Darooks <karol.jurczenia@gmail.com>
2018-02-06 18:01:46 +0100

    Update defects functionality

---
+++ api/defects/update_defects.php
@@ -0,0 +1,34 @@
+<?php
+
+require('../includes/dbconn.php');
+
+$id_defect = $_POST["id"];
+$new_title = $_POST["title"];
+$new_description = $_POST["description"];
+$new_status = $_POST["status"];
+$new_date = time();
+
+
+$query = "SELECT id_user FROM defects WHERE id_defect='$id_defect'";
+$result = mysqli_query($connection, $query) or die(mysqli_error($connection));
+
+$id_user = mysqli_fetch_array($result)["id_user"];
+
+if ($id_user == $_SESSION["id_user"]) {
+  $query = "UPDATE defects
+    SET title='$new_title', description='$new_description',
+      is_done='$new_status', date=FROM_UNIXTIME($new_date)
+        WHERE id_defect='$id_defect'";
+  if(mysqli_query($connection, $query) or die(mysqli_error($connection))) {
+    echo "true";
+  } else {
+    echo "false";
+  }
+} else {
+  echo "false";
+}
+
+
+// edytuj defekt
+
+ ?>
