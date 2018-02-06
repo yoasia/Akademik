a9970b2f5ae08352f6c82e3e701a94b73d46eedb
Darooks <karol.jurczenia@gmail.com>
2018-02-06 20:38:30 +0100

    Fixed typo in login script

--- api/login/login.php
+++ api/login/login.php
@@ -29,8 +29,8 @@
             $query = "SELECT id_user from users where email='$email'";
             $result_3 = mysqli_query($connection, $query) or die(mysqli_error($connection));
             $_SESSION["id_user"] = mysqli_fetch_assoc($result_3)["id_user"];
-            print json_encode($_SESSION);
-            include_once './get_login_staus.php';
+
+            include_once './get_login_status.php';
 
           } else {
             echo "Login ERROR: failed to parse the room number";
