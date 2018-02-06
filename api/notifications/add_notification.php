95fe7d78fead84647a4ffff06afef4bcddd35be7
yoasia <asiasien@gmail.com>
2018-02-06 22:14:46 +0100

    Small changes in notification script

--- api/notifications/add_notification.php
+++ api/notifications/add_notification.php
@@ -2,13 +2,13 @@
 include '../utils.php';
 require('../includes/dbconn.php');
 
-    $id_user = $_POST["id_user"];
+    $id_user = $_SESSION["id_user"];
     $title = $_POST["title"];
     $content = $_POST["content"];
     $now = time();
 
-    $query = "INSERT INTO notifications (id_user, content, date, time, title, ds_number)
-    VALUES ('$id_user', '$content', FROM_UNIXTIME($now), NOW(), '$title', $_SESSION["ds_number"])";
+    $query = "INSERT INTO notifications (id_user, content, date, time, title, ds_number) 
+    VALUES ('{$id_user}', '{$content}', FROM_UNIXTIME({$now}), NOW(), '{$title}', {$_SESSION["ds_number"]})";
 
     if(mysqli_query($mysqli, $query) or die(mysqli_error($mysqli)))
     {
