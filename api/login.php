80251239af3012d8e8606d90b788dba47787fd79
Darooks <karol.jurczenia@gmail.com>
2018-02-06 20:35:20 +0100

    Login is returning session in json format

--- api/login.php
+++ api/login.php
@@ -1,22 +1,22 @@
 <?php
 include_once 'db_connect.php';
 include_once 'functions.php';
- 
+
 sec_session_start(); // Our custom secure way of starting a PHP session.
- 
+
 if (isset($_GET['email'], $_GET['p'])) {
     $email = $_GET['email'];
     $password = $_GET['p']; // The hashed password.
- 
+
     if (login($email, $password, $mysqli) == true) {
-        // Login success 
+        // Login success
         header('Location: ./index.php');
     } else {
-        // Login failed 
+        // Login failed
         header('Location: ./?error=1');
     }
 } else {
-    // The correct POST variables were not sent to this page. 
+    // The correct POST variables were not sent to this page.
     echo 'Invalid Request';
 }
 
--- api/login/login.php
+++ api/login/login.php
@@ -36,7 +36,7 @@
             $query = "SELECT id_user from users where email='$email'";
             $result_3 = mysqli_query($connection, $query) or die(mysqli_error($connection));
             $_SESSION["id_user"] = mysqli_fetch_assoc($result_3)["id_user"];
-            echo "true";
+            print json_encode($_SESSION);
             include_once './get_login_staus.php';
 
           } else {
