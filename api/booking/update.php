7bbf071e05091b1324ee316b4810caa61f3cced1
Darooks <karol.jurczenia@gmail.com>
2018-02-07 18:21:21 +0100

    Booking checks users authorization

--- api/booking/update.php
+++ api/booking/update.php
@@ -5,28 +5,35 @@ session_start();
 
 $tablename = $_GET["tablename"];
 $id = $_GET["id"];
-$user_id = $_GET["user_id"];
 $action = $_GET["action"];
 
-if ($action == "book") {
-  $query = "UPDATE {$tablename}
-    SET id_user={$_SESSION['id_user']}
-      WHERE id_lock={$id}";
-}
-elseif ($action == "cancel") {
-  $query = "UPDATE {$tablename}
-    SET id_user=NULL
-      WHERE id_lock={$id}";
-}
-else {
-  die("Error");
-}
+$query = "SELECT id_user FROM {$tablename} WHERE id_lock={$id}";
+$id_result = mysqli_query($mysqli, $query) or die(mysqli_error($mysqli));
+$id_user = mysqli_fetch_assoc($id_result)["id_user"];
 
-$result = mysqli_query($mysqli, $query) or die(mysqli_error($mysqli));
-if ($result) {
-  print true;
+if ($_SESSION["id_user"] == $id_user) {
+  if ($action == "book") {
+    $query = "UPDATE {$tablename}
+      SET id_user={$_SESSION['id_user']}
+        WHERE id_lock={$id}";
+  }
+  elseif ($action == "cancel") {
+    $query = "UPDATE {$tablename}
+      SET id_user=NULL
+        WHERE id_lock={$id}";
+  }
+  else {
+    die("Error");
+  }
+
+  $result = mysqli_query($mysqli, $query) or die(mysqli_error($mysqli));
+  if ($result) {
+    print true;
+  } else {
+    print false;
+  }
 } else {
-  print false;
+  print "User is not authorized";
 }
 
  ?>
