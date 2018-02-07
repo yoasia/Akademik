a5a3a26ee7b999b85852be2cf5e385a0aa891c47
Darooks <karol.jurczenia@gmail.com>
2018-02-07 15:35:05 +0100

    Get gym lock partially working

--- api/gym_lock/get_gym_lock.php
+++ api/gym_lock/get_gym_lock.php
@@ -1,22 +1,73 @@
 <?php
 include '../utils.php';
 require('../includes/dbconn.php');
+session_start();
 
-    $query = "SELECT * FROM gym_lock";
+///////////////////////////////////////////
+// $_GET["ds_number"] = 2;
+// $_GET["floor"] = 3;
+///////////////////////////////////////////
+
+$actual_date = date('Y-m-d');
+$actual_hours = getHours();
+
+$ds_number = $_GET["ds_number"];
+$floor = $_GET["floor"];
+
+$gym->ds_number = $ds_number;
+$gym->hours = $actual_hours;
+
+$gym->days = array();
+$days->hours = array();
+
+for ($day_index = 0; $day_index < 7; $day_index++) {
+  if ($day_index != 0) {
+    $actual_date = date('Y-m-d', strtotime('+1 day', strtotime($actual_date)));
+  }
+  $day = date('l', strtotime($actual_date));
+
+  $days->name = $day;
+  $days->date = $actual_date;
+  $days->hours = array();
+
+  foreach ($actual_hours as $hour) {
+    $hour = parseHourToMYSQL($hour);
+
+    $query = "SELECT id_lock, id_user FROM gym_lock
+      WHERE date='{$actual_date}' AND floor={$floor} AND ds_number={$ds_number}";
     $result = mysqli_query($mysqli, $query) or die(mysqli_error($mysqli));
-    $json_array = array();
 
-    if($result > 0) {
-      //$result = json_encode($result);
-      while($row = mysqli_fetch_assoc($result)) {
-        $row["nickname"] = getNickname($row["id_user"]);
-        array_push($json_array, $row);
-      }
-      $json_array = json_encode($json_array);
-
+    $id_lock = mysqli_fetch_assoc($result)["id_lock"];
+    $id_user = mysqli_fetch_assoc($result)["id_user"];
+    print var_dump($id_user) . '<br/>';
+    if ($id_user == NULL) {
+      $room_number = NULL;
+      print $id_user;
     } else {
-      echo "false";
+      print "nie ma nulla";
+      $query = "SELECT room_number FROM users WHERE id_user={$id_user}";
+      $result_2 = mysqli_query($mysqli, $query) or die(mysqli_error($mysqli));
+
+      $room_number = mysqli_fetch_assoc($result_2)["room_number"];
     }
-    print $json_array;
+
+    $hours->id = $id_lock;
+    $hours->user_room = $room_number;
+    $hours->user_id = $id_user;
+
+    if($_SESSION["id_user"] == $id_user && $id_user != NULL) {
+      $hours->editable= true;
+    } else {
+      $hours->editable = false;
+    }
+
+    array_push($days->hours, $hours);
+  }
+  array_push($gym->days, $days);
+}
+
+$json_encoded = json_encode($gym);
+// var_dump($json_encoded);
+print $json_encoded;
 
  ?>
--- api/utils.php
+++ api/utils.php
@@ -35,4 +35,32 @@ function getFloorNumber($room_number) {
 
   return intval($room_number[0]);
 }
+
+function getHours() {
+  $hours[0] = "6:00";
+  $hours[1] = "7:00";
+  $hours[2] = "8:00";
+  $hours[3] = "9:00";
+  $hours[4] = "10:00";
+  $hours[5] = "11:00";
+  $hours[6] = "12:00";
+  $hours[7] = "13:00";
+  $hours[8] = "14:00";
+  $hours[9] = "15:00";
+  $hours[10] = "16:00";
+  $hours[11] = "17:00";
+  $hours[12] = "18:00";
+  $hours[13] = "19:00";
+  $hours[14] = "20:00";
+  $hours[15] = "21:00";
+  $hours[16] = "22:00";
+  $hours[17] = "23:00";
+  $hours[18] = "24:00";
+
+  return $hours;
+}
+
+function parseHourToMYSQL($hour) {
+  return $hour . ":00";
+}
 ?>
